e6ab9b1d2e88131c9aab6028aca2d054939f639f
Shahriar072000 <138397370+Shahriar072000@users.noreply.github.com>
2025-01-31 16:29:15 +0600

    Chat System

--- AiubLink/channels.Designer.cs
+++ AiubLink/channels.Designer.cs
@@ -39,6 +39,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.exitbutton = new System.Windows.Forms.Button();
             this.channelpanel = new System.Windows.Forms.Panel();
+            this.msgbox = new System.Windows.Forms.ListBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.channelpanel.SuspendLayout();
@@ -49,8 +50,9 @@
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.ForestGreen;
             this.flowLayoutPanel1.Controls.Add(this.Channelslabel);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(-1, -2);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1286, 80);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(964, 65);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
             // Channelslabel
@@ -58,9 +60,10 @@
             this.Channelslabel.AutoSize = true;
             this.Channelslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Channelslabel.ForeColor = System.Drawing.Color.Goldenrod;
-            this.Channelslabel.Location = new System.Drawing.Point(3, 0);
+            this.Channelslabel.Location = new System.Drawing.Point(2, 0);
+            this.Channelslabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Channelslabel.Name = "Channelslabel";
-            this.Channelslabel.Size = new System.Drawing.Size(289, 69);
+            this.Channelslabel.Size = new System.Drawing.Size(236, 55);
             this.Channelslabel.TabIndex = 0;
             this.Channelslabel.Text = "Channels";
             // 
@@ -69,18 +72,21 @@
             this.checkedListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.checkedListBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkedListBox.FormattingEnabled = true;
-            this.checkedListBox.Location = new System.Drawing.Point(-1, 76);
+            this.checkedListBox.Location = new System.Drawing.Point(-1, 62);
+            this.checkedListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkedListBox.Name = "checkedListBox";
-            this.checkedListBox.Size = new System.Drawing.Size(495, 644);
+            this.checkedListBox.Size = new System.Drawing.Size(371, 506);
             this.checkedListBox.TabIndex = 3;
             this.checkedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox_ItemCheck);
+            this.checkedListBox.SelectedIndexChanged += new System.EventHandler(this.checkedListBox_SelectedIndexChanged);
             // 
             // Assigmentbutton
             // 
             this.Assigmentbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Assigmentbutton.Location = new System.Drawing.Point(474, 13);
+            this.Assigmentbutton.Location = new System.Drawing.Point(356, 11);
+            this.Assigmentbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Assigmentbutton.Name = "Assigmentbutton";
-            this.Assigmentbutton.Size = new System.Drawing.Size(244, 67);
+            this.Assigmentbutton.Size = new System.Drawing.Size(183, 54);
             this.Assigmentbutton.TabIndex = 6;
             this.Assigmentbutton.Text = "Assigment";
             this.Assigmentbutton.UseVisualStyleBackColor = true;
@@ -90,19 +96,22 @@
             // 
             this.Sendbutton.BackColor = System.Drawing.Color.Orange;
             this.Sendbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Sendbutton.Location = new System.Drawing.Point(620, 505);
+            this.Sendbutton.Location = new System.Drawing.Point(465, 410);
+            this.Sendbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Sendbutton.Name = "Sendbutton";
-            this.Sendbutton.Size = new System.Drawing.Size(151, 40);
+            this.Sendbutton.Size = new System.Drawing.Size(113, 32);
             this.Sendbutton.TabIndex = 5;
             this.Sendbutton.Text = "Send";
             this.Sendbutton.UseVisualStyleBackColor = false;
+            this.Sendbutton.Click += new System.EventHandler(this.Sendbutton_Click);
             // 
             // Meetingbutton
             // 
             this.Meetingbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Meetingbutton.Location = new System.Drawing.Point(29, 13);
+            this.Meetingbutton.Location = new System.Drawing.Point(22, 11);
+            this.Meetingbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Meetingbutton.Name = "Meetingbutton";
-            this.Meetingbutton.Size = new System.Drawing.Size(269, 67);
+            this.Meetingbutton.Size = new System.Drawing.Size(202, 54);
             this.Meetingbutton.TabIndex = 4;
             this.Meetingbutton.Text = "Meeting";
             this.Meetingbutton.UseVisualStyleBackColor = true;
@@ -112,19 +121,22 @@
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(6, 505);
+            this.textBox1.Location = new System.Drawing.Point(4, 410);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(608, 41);
+            this.textBox1.Size = new System.Drawing.Size(456, 35);
             this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Controls.Add(this.Meetingbutton);
             this.panel1.Controls.Add(this.Assigmentbutton);
-            this.panel1.Location = new System.Drawing.Point(3, 552);
+            this.panel1.Location = new System.Drawing.Point(2, 448);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(785, 92);
+            this.panel1.Size = new System.Drawing.Size(589, 75);
             this.panel1.TabIndex = 7;
             // 
             // exitbutton
@@ -134,37 +146,53 @@
             this.exitbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.exitbutton.FlatAppearance.BorderSize = 0;
             this.exitbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitbutton.Location = new System.Drawing.Point(1224, 12);
+            this.exitbutton.Location = new System.Drawing.Point(918, 10);
+            this.exitbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exitbutton.Name = "exitbutton";
-            this.exitbutton.Size = new System.Drawing.Size(44, 41);
+            this.exitbutton.Size = new System.Drawing.Size(33, 33);
             this.exitbutton.TabIndex = 8;
             this.exitbutton.UseVisualStyleBackColor = false;
             this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
             // 
             // channelpanel
             // 
+            this.channelpanel.Controls.Add(this.msgbox);
             this.channelpanel.Controls.Add(this.panel1);
             this.channelpanel.Controls.Add(this.Sendbutton);
             this.channelpanel.Controls.Add(this.textBox1);
-            this.channelpanel.Location = new System.Drawing.Point(491, 76);
+            this.channelpanel.Location = new System.Drawing.Point(368, 62);
+            this.channelpanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.channelpanel.Name = "channelpanel";
-            this.channelpanel.Size = new System.Drawing.Size(794, 644);
+            this.channelpanel.Size = new System.Drawing.Size(595, 523);
             this.channelpanel.TabIndex = 9;
+            // 
+            // msgbox
+            // 
+            this.msgbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgbox.FormattingEnabled = true;
+            this.msgbox.ItemHeight = 16;
+            this.msgbox.Location = new System.Drawing.Point(7, 6);
+            this.msgbox.Name = "msgbox";
+            this.msgbox.Size = new System.Drawing.Size(576, 388);
+            this.msgbox.TabIndex = 8;
+            this.msgbox.SelectedIndexChanged += new System.EventHandler(this.msgbox_SelectedIndexChanged);
             // 
             // channels
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ClientSize = new System.Drawing.Size(960, 585);
             this.Controls.Add(this.channelpanel);
             this.Controls.Add(this.exitbutton);
             this.Controls.Add(this.checkedListBox);
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "channels";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "channels";
+            this.Load += new System.EventHandler(this.channels_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -186,5 +214,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button exitbutton;
         private System.Windows.Forms.Panel channelpanel;
+        private System.Windows.Forms.ListBox msgbox;
     }
 }
--- AiubLink/channels.cs
+++ AiubLink/channels.cs
@@ -182,5 +182,137 @@ namespace AiubLink
                 }
             }
         }
+
+        private void msgbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+  
+
+        private void SaveMessageToDatabase(string channelName, string senderID, string message)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\CS Final Project\AiubLink\DataBase\AiubLink.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=false";
+
+            string query = @"
+        INSERT INTO Messages (ChannelID, SenderID, MessageText, Timestamp) 
+        SELECT c.ChannelID, @SenderID, @MessageText, GETDATE()
+        FROM Channels c WHERE c.ChannelName = @ChannelName";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ChannelName", channelName);
+                        command.Parameters.AddWithValue("@SenderID", senderID);
+                        command.Parameters.AddWithValue("@MessageText", message);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving message: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void checkedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            msgbox.Items.Clear(); // Clear previous messages
+            if (checkedListBox.SelectedItem != null)
+            {
+                string selectedChannel = checkedListBox.SelectedItem.ToString();
+                LoadMessages(selectedChannel);
+            }
+        }
+
+        private void LoadMessages(string channelName)
+        {
+            msgbox.Items.Clear(); // Clear previous messages
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\CS Final Project\AiubLink\DataBase\AiubLink.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=false";
+
+            string query = @"
+    SELECT SenderID, MessageText, Timestamp
+    FROM Messages
+    INNER JOIN Channels ON Messages.ChannelID = Channels.ChannelID
+    WHERE Channels.ChannelName = @ChannelName
+    ORDER BY Timestamp ASC";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ChannelName", channelName);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (!reader.HasRows)
+                            {
+                                MessageBox.Show("No previous messages found.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+
+                            while (reader.Read())
+                            {
+                                string sender = reader["SenderID"].ToString();
+                                string message = reader["MessageText"].ToString();
+                                string time = Convert.ToDateTime(reader["Timestamp"]).ToString("HH:mm");
+
+                                msgbox.Items.Add($"[{time}] {sender}: {message}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading messages: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sendbutton_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a message.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (checkedListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a channel first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string selectedChannel = checkedListBox.SelectedItem.ToString();
+            string message = textBox1.Text.Trim();
+
+            // Save message to ListBox
+            msgbox.Items.Add($"{userID}: {message}");
+
+            // Optional: Save to database
+            SaveMessageToDatabase(selectedChannel, userID, message);
+
+            // Clear the input box
+            textBox1.Clear();
+        }
+
+        private void channels_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
